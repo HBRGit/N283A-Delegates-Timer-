9d9caa26ea9f9617b7dc98ed1bbaf42aa9c33608
HBRGit <157648097+HBRGit@users.noreply.github.com>
2025-10-08 12:35:37 +0200

    Add files via upload

---
+++ N283A/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+System.Timers.Timer timer = new System.Timers.Timer(1000); // 1 second interval
+
+//timer.Elapsed += (sender, e) => Console.WriteLine($"Timer elapsed at {DateTime.Now}");  // Using lambda expression
+// or
+timer.Elapsed +=OnTimedEvent;   // Using method group conversion
+
+timer.AutoReset = true; // Repeatable timer
+timer.Start();
+
+Console.WriteLine("Timer started. Press any key to exit the program...");
+Console.ReadKey();
+void OnTimedEvent(object source, System.Timers.ElapsedEventArgs e)
+{
+    Console.WriteLine($"Timer elapsed at {DateTime.Now}");
+}
+
+
